309e10c6e0d7074ca0a8628a7b92ce3c5a80dd87
treavorg <treavorg@ssicanada.com>
2022-11-25 14:31:02 -0800

    refactoring

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const port = process.env.PORT || 5000;
-const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_TOKEN);
+const client = require('twilio')(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_TOKEN
+);
 const bodyParser = require('body-parser');
 const Gen2FA = require('./Gen2FA');
-const gen = new Gen2FA(2, (4*60*1000), '123abc');
+const gen = new Gen2FA(2, 4 * 60 * 1000, '123abc');
 const app = express();
 const cors = require('cors');
-app.use(cors()); 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/sendsms', (req, res) => {
@@ -18,19 +21,20 @@ app.post('/sendsms', (req, res) => {
       from: '+18563865091',
       to: req.body.receiver
     })
-    .then(message => {
-      console.log(message.sid)
+    .then((message) => {
+      console.log(message.sid);
       res.end();
     });
 });
 
 app.post('/token-verify', (req, res) => {
-  const result = gen.check2FA(req.body.token, req.body.uid)
+  const result = gen.check2FA(req.body.token, req.body.uid);
   console.log(req.body, result);
   if (result === true) {
-      res.status(200).send({ cookie: 'authorized', msg: 'PASSCODE ACCEPTED' });
+    // res.redirect(301, 'https://rococo-chaja-8dbc41.netlify.app');
+    res.status(200).send({ cookie: 'authorized', msg: 'PASSCODE ACCEPTED' });
   } else {
-      res.status(404).send('PASSCODE INCORRECT');
+    res.status(404).send('PASSCODE INCORRECT');
   }
 });
 
@@ -38,16 +42,16 @@ app.post('/token-send', (req, res) => {
   const token = gen.make2FA(req.body.uid);
   client.messages
     .create({
-      body: req.body.uid + " your token is " + token,
+      body: req.body.uid + ' your token is ' + token,
       from: '+18563865091',
       to: req.body.receiver
     })
-    .then(message => {
-      console.log(message.sid)
+    .then((message) => {
+      console.log(message.sid);
       res.status(200).send(`CODE SENT ${token}`).end();
     });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
